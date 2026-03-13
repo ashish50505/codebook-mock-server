8110f0d1dc9a5cffaa69658af7d8d50ecc077286
fantasyxpert <ashishmaurya397@gmail.com>
2026-03-13 21:14:08 +0530

    remove cors

--- index.js
+++ index.js
@@ -1,35 +1,29 @@
 import express from "express";
 import jsonServer from "json-server";
 import auth from "json-server-auth";
-import cors from "cors";
 
 const server = express();
-
-server.use(cors());
-
 server.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "*");
-  next();
-});
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Headers', '*')
+    next()
+})
 
-const router = jsonServer.router("./data/db.json");
-server.db = router.db;
+const router = jsonServer.router('./data/db.json');
+server.use('/api', router);
+server.db = router.db
 
-const middlewares = jsonServer.defaults();
-
+const middlewares = jsonServer.defaults()
 const rules = auth.rewriter({
-  products: 444,
-  featured_products: 444,
-  orders: 660,
-  users: 600,
+    products: 444,
+    featured_products: 444,
+    orders: 660,
+    users: 600
 });
 
-server.use(middlewares);
-server.use(rules);
-server.use(auth);
-server.use(router);
+server.use(rules)
+server.use(auth)
+server.use(middlewares)
+server.use(router)
 
-server.listen(8000, () => {
-  console.log("Mock server running on port 8000");
-});
+server.listen(8000);
