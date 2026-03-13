aaff0e941eaf957bcc62e423269dabaed07d1bb8
fantasyxpert <ashishmaurya397@gmail.com>
2026-03-13 21:02:51 +0530

    Fix server setup and cors

--- index.js
+++ index.js
@@ -1,29 +1,35 @@
 import express from "express";
 import jsonServer from "json-server";
 import auth from "json-server-auth";
+import cors from "cors";
 
 const server = express();
+
+server.use(cors());
+
 server.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*')
-    res.header('Access-Control-Allow-Headers', '*')
-    next()
-})
-
-const router = jsonServer.router('./data/db.json');
-server.use('/api', router);
-server.db = router.db
-
-const middlewares = jsonServer.defaults()
-const rules = auth.rewriter({
-    products: 444,
-    featured_products: 444,
-    orders: 660,
-    users: 600
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "*");
+  next();
 });
 
-server.use(rules)
-server.use(auth)
-server.use(middlewares)
-server.use(router)
+const router = jsonServer.router("./data/db.json");
+server.db = router.db;
 
-server.listen(8000);
+const middlewares = jsonServer.defaults();
+
+const rules = auth.rewriter({
+  products: 444,
+  featured_products: 444,
+  orders: 660,
+  users: 600,
+});
+
+server.use(middlewares);
+server.use(rules);
+server.use(auth);
+server.use(router);
+
+server.listen(8000, () => {
+  console.log("Mock server running on port 8000");
+});
